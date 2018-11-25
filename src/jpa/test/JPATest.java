f513c341011ff2b9b44c72c6e657b42eee0591bb
cosisal <cosisal@libero.it>
2018-11-25 12:16:14 +0100

    Aggiunta update e rollback

--- src/jpa/test/JPATest.java
+++ src/jpa/test/JPATest.java
@@ -17,7 +17,7 @@ public class JPATest {
 		List<Anagrafica> anagraficaList = entityManager.createQuery("SELECT a FROM anagrafica a").getResultList();
 		
 		for (Anagrafica anagrafica : anagraficaList) {
-			System.out.println(anagrafica.getCodiceFiscale());
+			System.out.println(anagrafica.getNome());
 		}
 		
 		Anagrafica giorgia = new Anagrafica();
@@ -27,15 +27,30 @@ public class JPATest {
 		
 		entityManager.persist(giorgia);
 		
+		anagraficaList = entityManager.createQuery("SELECT a FROM anagrafica a").getResultList();
+		
+		for (Anagrafica anagrafica : anagraficaList) {
+			System.out.println(anagrafica.getNome());
+		}
+		
+		
+		giorgia = entityManager.find(Anagrafica.class, "CSOGRG16D64A669U");
+		giorgia.setNome("Giorgina");
+		
+		anagraficaList = entityManager.createQuery("SELECT a FROM anagrafica a").getResultList();
+		
+		for (Anagrafica anagrafica : anagraficaList) {
+			System.out.println(anagrafica.getNome());
+		}
+		
 		entityManager.getTransaction().rollback();
 		
 		anagraficaList = entityManager.createQuery("SELECT a FROM anagrafica a").getResultList();
 		
 		for (Anagrafica anagrafica : anagraficaList) {
-			System.out.println(anagrafica.getCodiceFiscale());
+			System.out.println(anagrafica.getNome());
 		}
-		
-		entityManager.getTransaction().commit();
+//		entityManager.getTransaction().commit();
 		entityManager.close();
 		jpaEntityManagerFactory.factoryShutDown();
 
